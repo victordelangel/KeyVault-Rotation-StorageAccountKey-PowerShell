cf62a6c86f864a6f0544bf55b71878eb950056da
Víctor Hugo Del Angel Ramos <videla@microsoft.com>
2020-12-16 11:10:44 -0600

    Update run.ps1
    added connection string

--- AKVStorageRotation/run.ps1
+++ AKVStorageRotation/run.ps1
@@ -9,8 +9,9 @@ function RegenerateKey($keyId, $providerAddress){
     #Regenerate key 
     New-AzStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName -KeyName $keyId
     $newKeyValue = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -AccountName $storageAccountName|where KeyName -eq $keyId).value
-
-    return $newKeyValue
+    $newConnectionString = "DefaultEndpointsProtocol=https;AccountName=dsoohlite3sa;AccountKey="+ $newKeyValue + ";EndpointSuffix=core.windows.net"
+    
+    return $newConnectionString
 }
 
 function AddSecretToKeyVault($keyVAultName,$secretName,$newAccessKeyValue,$exprityDate,$tags){
